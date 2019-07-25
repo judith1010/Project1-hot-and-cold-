5745fa9190cec9d9801c4a1c87afa647a56feea6
Judith <judithsw11@gmail.com>
2019-07-25 15:48:13 -0400

    7/25/19

---
+++ index.js
@@ -0,0 +1,54 @@
+
+
+var box = document.getElementById('box');
+var boxHeight = box.offsetHeight;
+var boxWidth = box.offsetWidth;
+
+var targetX = Math.random() * boxWidth;
+var targetY = Math.random() * boxHeight;
+console.log(`targetX: ${targetX}`)
+console.log(`targetY: ${targetY}`)
+
+function distance(x1, x2, y1, y2){
+  var x = x1 - x2;
+  var y = y1 - y2;
+  x *= x;
+  y *= y;
+  return Math.sqrt(x + y);
+}
+
+var win = false;
+function loc(e){
+  if (win === false){
+    // console.log(`mouseX: ${event.clientX}`)
+    var xMouse = e.clientX;
+    var yMouse = e.clientY;
+    var d = distance(xMouse, targetX, yMouse, targetY);
+    if (d <= 40){
+      box.style.backgroundColor = "rgb(0,0,255)";
+      win = true;
+      // alert("You win! Refresh to play again")
+    }
+    else{
+      box.style.backgroundColor = `rgb(0,${255-d},0)`;
+    }
+  }
+}
+
+box.addEventListener("mousemove", loc)
+
+
+
+// var mouseX = box.addEventListener("mousemove", locX);
+// var mouseY = box.addEventListener("mousemove", locY);
+
+// var d = distance(mouseX, targetX, mouseY, targetY)
+// console.log(d)
+// box.addEventListener()
+
+// function background(){
+//   box.style.backgroundColor = `rgb(0,${255-distance(mouseX, targetX, mouseY, targetY)},0)`
+// }
+// box.addEventListener("mousemove", background)
+
+// box.style.backgroundColor = `rgb(0,${255-distance(mouseX, targetX, mouseY, targetY)},0)`
